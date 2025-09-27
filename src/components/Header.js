5b39a8b36ca50a5c82acbb040b51e9f55abbd4b5
suraj gupta <baniyasuraj143@gmail.com>
2025-09-27 13:28:30 +0530

    Updated UI and fixed Services page

--- src/components/Header.js
+++ src/components/Header.js
@@ -168,7 +168,7 @@ function Header() {
               <Nav.Link as={Link} to="/">Services</Nav.Link>
               <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
               <Nav.Link as={Link} to="/bridal">Bridal</Nav.Link>
-              <Nav.Link as={Link} to="/runway-rewards">Runway Rewards</Nav.Link>
+              <Nav.Link as={Link} to="/RunwayRewads">Runway Rewards</Nav.Link>
               <Nav.Link as={Link} to="/offer">Offer</Nav.Link>
               <Nav.Link as={Link} to="/french">Franchise</Nav.Link>
               <Nav.Link as={Link} to="/locator">Salon Locator</Nav.Link>
